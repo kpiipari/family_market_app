dffc03590a4c8c58eff4cd7561dda2da9bf6d8f6
kaisa <kaisa.piipari@gmail.com>
2017-10-25 21:03:45 +0100

    Creates a market controller. Adds market#index as a root route. Adds items resources as a nested resource to categories resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :item_tags
   resources :items
-  resources :categories
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :categories do
+      resources :items
+  end
+  
+  root 'market#index'
+
 end
