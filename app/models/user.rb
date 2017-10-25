6ca8abf592f1d213ae59b5b3b2fd087a0d947bef
kaisa <kaisa.piipari@gmail.com>
2017-10-26 00:05:02 +0100

    Adds associations to user and item_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :item_users
+  has_many :items, through: :item_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
