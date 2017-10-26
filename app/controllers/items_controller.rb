ad10af60eb4cc2c778f8cdbe62fcdd4da9979daa
kaisa <kaisa.piipari@gmail.com>
2017-10-26 23:11:30 +0100

    Adds seed data for categories and seeds the database. Started working on new item form.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+    before_filter :authenticate_user!, :except => [:show, :index]
 
     def show
     end
--- app/controllers/market_controller.rb
+++ app/controllers/market_controller.rb
@@ -1,6 +1,6 @@
 class MarketController < ApplicationController
 
     def index
-
+        @users = User.all
     end
 end
