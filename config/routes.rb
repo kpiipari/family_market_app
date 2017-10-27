9b6487b188a8e04c745497ba47656e15327979bd
kaisa <kaisa.piipari@gmail.com>
2017-10-27 23:47:19 +0100

    Adds user show resource route. Adds 'Add Item' link to user show page. Adds a link to current user's home page on the index page. Updates show action in users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :item_users
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  resources :users, :only => [:show]
   resources :item_tags
   resources :items
-  get '/users/:id', to: 'users#show'
   
   resources :categories do
       resources :items
