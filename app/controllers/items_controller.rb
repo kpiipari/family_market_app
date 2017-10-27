5a7ccf3c78a7a7e4d3c3663965ee57a4a4651f4a
kaisa <kaisa.piipari@gmail.com>
2017-10-27 23:16:34 +0100

    Adds links to items on the index page. Adds show action in items controller. Creates a basic items show view. Adds a heading for categories show page.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
     before_action :authenticate_user!, :except => [:show, :index]
 
     def show
+        @item = Item.find(params[:id])
     end
     
     def new
