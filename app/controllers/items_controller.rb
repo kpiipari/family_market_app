03f03cb6de13142c6d786db37c9a0bc48b0f10df
kaisa <kaisa.piipari@gmail.com>
2017-11-01 20:42:06 +0000

    Removes binding.pry

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,6 @@ class ItemsController < ApplicationController
     def create
         @user = current_user
         @item = Item.create(item_params)
-        binding.pry
         @item_user = ItemUser.create(user_id: @user.id, item_id: @item.id)
         redirect_to root_path
     end
