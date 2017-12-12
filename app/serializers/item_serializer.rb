9d91bad778a4e2ccb4850e20d699e6638656484b
kaisa <kaisa.piipari@gmail.com>
2017-12-12 09:04:48 +0000

    Adds tag serializer. Updates serializer associations between item and tag serializers.

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :status, :category_id
   belongs_to :category, serializer: CategorySerializer
+  has_many :tags
 end
