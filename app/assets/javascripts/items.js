50d4cbe4e424cd473335f5c4e8abdcdbbc68dddb
kaisa <kaisa.piipari@gmail.com>
2017-12-13 21:41:54 +0000

    Adds Item.done and Item.error instance methods to handle item form submissions success and failure. Splits new item form submission to a newItemFormSubmit method that is called by the new item form event listener.

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -5,19 +5,39 @@ function Item(item) {
     this.category_id = item.category_id
 }
 
-$(function() {
-    $("form#new_item").on("submit", function(e){
-        e.preventDefault();
-        var $form = $(this);
-        var action = $form.attr("action")
-        var params = $form.serialize();
-        var posting = $.post(action, params);
-        
-        posting.done(function(data) {
-            var item = new Item(data)
-            var itemLi = item.buildItemLi();
-            $("ul.market").append(itemLi);
-            console.log(item)
-        })
+Item.done = function(response){
+    var item = new Item(response)
+    var itemLi = item.buildItemLi();
+    $("ul.market").append(itemLi);
+    console.log(item)
+}
+
+Item.fail = function(response) {
+    console.log("Problem submitting the form", response)
+    alert("There was a problem submitting the form. Please try again.")
+}
+
+
+
+Item.prototype.buildItemLi = function() {
+
+}
+
+Item.newItemFormSubmit = function(e){
+    e.preventDefault();
+    var $form = $(this);
+    var action = $form.attr("action")
+    var params = $form.serialize();
+    var posting = $.post(action, params);
+    
+    posting.done(function(data) {
+        var item = new Item(data)
+        var itemLi = item.buildItemLi();
+        $("ul.market").append(itemLi);
+        console.log(item)
     })
+}
+
+$(function() {
+    $("form#new_item").on("submit", Item.newItemFormSubmit)
 })
