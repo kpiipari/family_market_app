eac4bfdd818796c84542edeb46cd870f64ffef81
kaisa <kaisa.piipari@gmail.com>
2017-12-14 09:12:54 +0000

    Adds UserSerializer. Updates item_serializer to belong to UserSerializer.

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,6 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :status, :category_id
   belongs_to :category, serializer: CategorySerializer
+  belongs_to :user, serializer: UserSerializer
   has_many :tags
 end
