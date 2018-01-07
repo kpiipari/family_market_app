376d1b95e30d7043559861899b252e7306483d8c
kaisa <kaisa.piipari@gmail.com>
2018-01-07 00:57:33 +0000

    Updates CategoryController index action to render json

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,10 @@ class CategoriesController < ApplicationController
 
     def index
         @categories = Category.all 
+        respond_to do |format|
+            format.json { render json: @categories, each_serializer: CategorySerializer }
+            format.html 
+        end
     end
     
     def show
